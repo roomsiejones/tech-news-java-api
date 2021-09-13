0de105a5cbd32b473770aebc3fbf5c4b80e8b685
Roomsie Jones <seangravito@gmail.com>
2021-09-12 22:54:53 -0500

    added a webrunner plug-in

--- src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TechNewsJavaApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TechNewsJavaApiApplication.class, args);
+
 	}
 
 }
